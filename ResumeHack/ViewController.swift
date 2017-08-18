dbefd48fec8ac9cf1d9ff68ea2f91f7d4de0b2cc
Ray Gao <atlantis.ray@gmail.com>
2017-08-19 00:10:24 +0800

    Initial Commit

---
+++ ResumeHack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ResumeHack
+//
+//  Created by 高睿 on 17/8/19.
+//  Copyright © 2017年 Ray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
